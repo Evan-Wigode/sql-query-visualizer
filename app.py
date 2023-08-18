81d569dc53b07b9b3fd4abf429f70ca0dbe91617
Evan-Wigode <evanwig12@outlook.com>
2023-08-18 19:15:42 -0400

    setup frontend interfacing with backend and adding UI to frontend

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 CORS(app)  # This is to allow requests from your React frontend
 
 DATABASE = 'sample.db'
-
+    
 @app.route('/execute', methods=['POST'])
 def execute_query():
     query = request.json['query']
@@ -25,6 +25,8 @@ def execute_query():
         conn = sqlite3.connect(DATABASE)
         cursor = conn.cursor()
         cursor.execute(query)
+        # Commit changes to the database. Important for INSERT, UPDATE, DELETE
+        conn.commit()
         results = cursor.fetchall()
         conn.close()
 
@@ -32,6 +34,7 @@ def execute_query():
 
     except sqlite3.Error as e:
         return jsonify({'status': 'error', 'message': str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,63 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Container, TextField, Button, Paper, Typography, Box } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/execute', { query });
+      setResults(response.data);
+    } catch (error) {
+      console.error("There was an error executing the query:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>SQL Query Visualizer</h1>
-    </div>
+    <Container maxWidth="md">
+      <Box my={4}>
+        <Typography variant="h4" gutterBottom>
+          SQL Query Visualizer
+        </Typography>
+
+        <Paper elevation={3} style={{ padding: '20px' }}>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              multiline
+              rows={6}
+              variant="outlined"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Type your SQL query here..."
+              margin="normal"
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<SendIcon />}
+              type="submit"
+            >
+              Execute
+            </Button>
+          </form>
+        </Paper>
+
+        {results && (
+          <Paper elevation={3} style={{ marginTop: '20px', padding: '20px' }}>
+            <Typography variant="h6" gutterBottom>
+              Results:
+            </Typography>
+            <pre>{JSON.stringify(results, null, 2)}</pre>
+          </Paper>
+        )}
+      </Box>
+    </Container>
   );
 }
-
 
 export default App;
