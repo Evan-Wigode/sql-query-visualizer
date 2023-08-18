97044520f85df8c204efd806ffc2d9859541e5cf
EvanWigode <eiw8tpq@virginia.edu>
2023-08-18 16:34:25 -0400

    Set up basic Flask backend and React frontend

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import sqlite3
+
+conn = sqlite3.connect('sample.db')
+print("Opened database successfully")
+
+conn.execute('''CREATE TABLE IF NOT EXISTS SAMPLE
+        (ID INT PRIMARY KEY NOT NULL,
+        NAME TEXT NOT NULL);''')
+print("Table created successfully")
+
+conn.close()
+
+app = Flask(__name__)
+CORS(app)  # This is to allow requests from your React frontend
+
+DATABASE = 'sample.db'
+
+@app.route('/execute', methods=['POST'])
+def execute_query():
+    query = request.json['query']
+    
+    try:
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+        conn.close()
+
+        return jsonify({'status': 'success', 'data': results})
+
+    except sqlite3.Error as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
